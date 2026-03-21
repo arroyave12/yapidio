6ba50966220d9f324ce29e85d025310ec3506d47
arroyave12 <58708912+arroyave12@users.noreply.github.com>
2026-03-21 08:16:02 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'yapidio',
+  webDir: 'www'
+};
+
+export default config;
